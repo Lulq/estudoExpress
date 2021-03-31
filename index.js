dd20dcf1c8b46d38ddbc1ca318736d84f1a91f7d
Luiz Negrinho <luizavm@gmail.com>
2021-03-31 12:02:58 -0300

    metodos http

--- index.js
+++ index.js
@@ -13,7 +13,13 @@ const app = express();
 // })
 
 app.get('/projects', (request, response) => {
+    const {title, owner} = request.query;
+    
+    console.log(title);
+    console.log(owner);
+
     return response.json([
+
         'Projeto 1',
         'Projeto 2',
         'Projeto 100'
@@ -30,6 +36,30 @@ app.post('/projects', (request, response) => {
 
 })
 
+app.put('/projects/:id', (request, response) => {
+    
+    const params = request.params;
+
+    console.log(params);
+
+    return response.json([
+        'Projeto 50',
+        'Projeto 2',
+        'Projeto 3',
+        'Projeto 4',
+        'Projeto 5'
+    ])
+})
+
+app.delete('/projects/:id', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3',
+        'Projeto 4',
+        'Projeto 5'
+    ])
+})
 
 app.listen(3333);
 
