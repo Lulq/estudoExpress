9f7d2f6be6cb431f74a92a4704450fe17aa83105
Luiz Negrinho <luizavm@gmail.com>
2021-03-31 11:01:40 -0300

    instalado o nodemon, criado o servidor, o get e post

--- index.js
+++ index.js
@@ -2,4 +2,37 @@ const express = require('express');
 
 const app = express();
 
-console.log(app);
+// console.log(app);
+
+//criando uma rota
+
+
+
+// app.get('/', (request, response) => {
+//     response.send('Olá, Mundo!');
+// })
+
+app.get('/projects', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 100'
+    ])
+});
+
+app.post('/projects', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3',
+        'Projeto 4'
+    ])
+
+})
+
+
+app.listen(3333);
+
+// app.listen(3000, () => {
+//     console.log('Servidor rodando!')
+// })
